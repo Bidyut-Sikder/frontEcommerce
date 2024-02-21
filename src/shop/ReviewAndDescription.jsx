de8ffecc58ce554a78dfe62f3464ad70d811bf53
Bidyut Sikder <89535239+Bidyut-Sikder@users.noreply.github.com>
2024-02-21 22:28:19 +0600

    Update ReviewAndDescription.jsx

--- src/shop/ReviewAndDescription.jsx
+++ src/shop/ReviewAndDescription.jsx
@@ -7,28 +7,28 @@ const reviwtitle = "Add a Review";
 
 const ReviewList = [
     {
-        imgUrl: "/src/assets/images/instructor/01.jpg",
+        imgUrl: "https://github.com/Bidyut-Sikder/frontEcommerce/blob/master/src/assets/images/instructor/01.jpg?raw=true",
         imgAlt: "Client thumb",
         name: "Ganelon Boileau",
         date: "Posted on Jun 10, 2022 at 6:57 am",
         desc: "Enthusiast build innovativ initiatives before lonterm high-impact awesome theme seo psd porta monetize covalent leadership after without resource.",
     },
     {
-        imgUrl: "/src/assets/images/instructor/02.jpg",
+        imgUrl: "https://github.com/Bidyut-Sikder/frontEcommerce/blob/master/src/assets/images/instructor/02.jpg?raw=true",
         imgAlt: "Client thumb",
         name: "Morgana Cailot",
         date: "Posted on Jun 10, 2022 at 6:57 am",
         desc: "Enthusiast build innovativ initiatives before lonterm high-impact awesome theme seo psd porta monetize covalent leadership after without resource.",
     },
     {
-        imgUrl: "/src/assets/images/instructor/03.jpg",
+        imgUrl: "https://github.com/Bidyut-Sikder/frontEcommerce/blob/master/src/assets/images/instructor/03.jpg?raw=true",
         imgAlt: "Client thumb",
         name: "Telford Bois",
         date: "Posted on Jun 10, 2022 at 6:57 am",
         desc: "Enthusiast build innovativ initiatives before lonterm high-impact awesome theme seo psd porta monetize covalent leadership after without resource.",
     },
     {
-        imgUrl: "/src/assets/images/instructor/04.jpg",
+        imgUrl: "https://github.com/Bidyut-Sikder/frontEcommerce/blob/master/src/assets/images/instructor/04.jpg?raw=true",
         imgAlt: "Client thumb",
         name: "Cher Daviau",
         date: "Posted on Jun 10, 2022 at 6:57 am",
