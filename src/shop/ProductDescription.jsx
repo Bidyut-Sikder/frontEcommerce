2085f4beb5aeebf33e4d37e6c36fc4311a643bc5
Bidyut Sikder <89535239+Bidyut-Sikder@users.noreply.github.com>
2024-02-21 22:30:31 +0600

    Update ProductDescription.jsx

--- src/shop/ProductDescription.jsx
+++ src/shop/ProductDescription.jsx
@@ -6,7 +6,7 @@ const ProductDescription = () => {
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id eos architecto cum ea ipsum? Veritatis quam reiciendis deleniti nihil nam quidem, quis tenetur labore, placeat vitae ipsam eveniet maxime eaque iste in saepe temporibus! Consequuntur, explicabo accusantium doloribus neque repellat ut illum aspernatur, iure hic iusto nisi quibusdam architecto officia.</p>
             <div className="post-item">
                 <div className="post-thumb">
-                    <img src="/src/assets/images/shop/01.jpg" alt="" />
+                    <img src="https://github.com/Bidyut-Sikder/frontEcommerce/blob/master/src/assets/images/shop/01.jpg?raw=true" alt="" />
                 </div>
                 <div className="post-content">
                     <ul className="lab-ul">
